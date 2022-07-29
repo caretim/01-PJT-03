f8fb3e413b6a21ef1bfb6febd47a209abc9ec78a
caretim <karesin72041@gmail.com>
2022-07-29 18:44:42 +0900

    암호문 시험후 마무리

--- 2회차/하승찬/6_암호문1.py
+++ 2회차/하승찬/6_암호문1.py
@@ -1,25 +1,36 @@
 import sys
 
 sys.stdin = open("_암호문1.txt")
-n_ori_len =int(input())
 
-ori_num=[]
-ori_num=list(map(int,input().split()))
+for test_case in range(1, 11):
 
-n_oder =int(input())
+    n_ori_len =int(input())
 
-oder_list=[]
-oder_list=list(map(str,input().split()))
+    ori_num=[]
+    ori_num=list(map(str,input().split()))
 
-rnum = []
+    n_oder =int(input())
 
-for i in range(len(oder_list)):
-    if oder_list[i] == 'I':
-        x= int(oder_list[i+1])
-        y= int(oder_list[i+2])
-        rnum.append(oder_list[i+3:i+3+y]) 
-        for j in rnum[0]:
-            ori_num.insert(x,j)
-        rnum.clear()
-print(ori_num)
+    oder_list=[]
+    oder_list=list(map(str,input().split()))
+
+    rnum = []
+
+    for i in range(len(oder_list)):
+        if oder_list[i] == 'I':
+            x= int(oder_list[i+1])
+            y= int(oder_list[i+2])
+            rnum.append(oder_list[i+3:i+3+y]) 
+            cnt=0
+            for j in rnum[0]:
+                ori_num.insert(x+cnt,j)
+                cnt+=1
+            rnum.clear()
+
+    reselt_list =[]
+    for reselt in range(0,10):
+        reselt_list.append(str(ori_num[reselt]))
+    r=' '.join(reselt_list)
+    print(f'#{test_case} {r}')
+
 
